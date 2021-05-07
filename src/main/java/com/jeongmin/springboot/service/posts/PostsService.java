bbebfc290dfc6d5e25a312c4acde33ffc9416cad
DESKTOP-1Q5QTS0\user <wudals0708@naver.com>
2021-05-07 09:25:23 +0900

    전체 조회 화면 만들기

--- src/main/java/com/jeongmin/springboot/service/posts/PostsService.java
+++ src/main/java/com/jeongmin/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.jeongmin.springboot.service.posts;
 
 import com.jeongmin.springboot.domain.posts.Posts;
 import com.jeongmin.springboot.domain.posts.PostsRepository;
+import com.jeongmin.springboot.web.dto.PostsListResponseDto;
 import com.jeongmin.springboot.web.dto.PostsResponseDto;
 import com.jeongmin.springboot.web.dto.PostsSaveRequestDto;
 import com.jeongmin.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +36,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()
                 -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional//(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
 
     }
+
 }
